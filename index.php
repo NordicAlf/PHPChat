eb0e814dbdca73554143f44e42bb34f6c75e176c
North-Guard <sver4ok@protonmail.com>
2020-04-21 21:01:58 +0300

    create: design for app

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Веб-чат</title>
+    <link href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container col-lg">
+        <div class="application">
+            <div class="row">
+
+                <!--Левая панель со списком пользователей-->
+                <div class="user_panel col-lg-3">
+                    <div class="row">
+                        <div class="info_user">
+                            <div class="avatar_user"></div>
+                            <div class="name_user">Имя Фамилия</div>
+                            <div class="name_nick">Crack_man</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="user">
+                            <div class="avatar"></div>
+                            <div class="nick">Crack_man</div>
+                        </div>
+                        <div class="user">
+                            <div class="avatar"></div>
+                            <div class="nick">Crack_man</div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Правая панель с сообщениями-->
+                <div class="message_panel col-lg-9">
+                    <div class="message">
+                        <div class="row">
+                            <div class="message_avatar col-lg-1"></div>
+                            <div class="message_nick col-lg-2">Crack_man</div>
+                        </div>
+                        <div class="row">
+                            <div class="message_text col-lg-10">
+                                Здесь текст сообщенияЗдесь текст сообщения
+                                Здесь текст сообщенияЗдесь текст сообщенияЗдесь текст сообщения
+                                Здесь текст сообщения
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="message_date col-lg-12">
+                                20 февраля 2020
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="message">
+                        <div class="row">
+                            <div class="message_avatar col-lg-1"></div>
+                            <div class="message_nick col-lg-2">Crack_man</div>
+                        </div>
+                        <div class="row">
+                            <div class="message_text col-lg-10">
+                                Здесь текст сообщенияЗдесь текст сообщения
+                                Здесь текст сообщенияЗдесь текст сообщенияЗдесь текст сообщения
+                                Здесь текст сообщения
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="message_date col-lg-12">
+                                20 февраля 2020
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
